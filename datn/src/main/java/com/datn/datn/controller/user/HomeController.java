ed13ca224c628c239d6413ce617d40eac2d7d250
DinhHuynhDuc <dinhduc9977@gmail.com>
2025-06-27 18:26:32 +0700

    Update HomeController.java

--- datn/src/main/java/com/datn/datn/controller/user/HomeController.java
+++ datn/src/main/java/com/datn/datn/controller/user/HomeController.java
@@ -1,4 +1,4 @@
-package com.datn.datn.controller.user
+package com.datn.datn.controller.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
